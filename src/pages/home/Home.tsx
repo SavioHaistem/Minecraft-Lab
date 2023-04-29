2f406f30067bb460bfe05740c096e7249dad80be
Sávio Haistém <savio.dantas.5858@gmail.com>
2023-04-29 18:01:58 -0300

    Adicionando styled-component home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,13 @@
 import HomeFigures from "../../components/HomeFigures";
+import StyledHome from "../../css/StyledHome";
 
 function Home() {
   return (
   <>
+    <StyledHome>
       <HomeFigures/>
-        <h1> Home </h1>
+        <h1> Minecraft Lab </h1>
+    </StyledHome>
   </>
   );
 }
